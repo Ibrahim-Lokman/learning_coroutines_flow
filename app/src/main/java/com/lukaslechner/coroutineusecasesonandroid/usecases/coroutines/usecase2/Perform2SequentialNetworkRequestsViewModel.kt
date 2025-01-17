f47e3ac6d72e175d0c567bda57a341ab0e0301a3
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2025-01-17 20:37:53 +0600

    Async Programming: coroutine Implementation.

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,7 +1,9 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
@@ -10,5 +12,18 @@ class Perform2SequentialNetworkRequestsViewModel(
     fun perform2SequentialNetworkRequest() {
         // TODO: Exercise 1
         // switch to branch "coroutine_course_full" to see solution
+
+        uiState.value = UiState.Loading
+
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersions = mockApi.getRecentAndroidVersions()
+                val mostRecentVersion = recentAndroidVersions.last()
+                val features = mockApi.getAndroidVersionFeatures(mostRecentVersion.apiLevel)
+                uiState.value = UiState.Success(features)
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error("Network Request failed")
+            }
+        }
     }
 }
