225bbdb76e2c08ca1ebdf79b327e3686452be15a
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2025-01-24 13:11:26 +0600

    run blocking used

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/coroutinebuilder/1_launch.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/coroutinebuilder/1_launch.kt
@@ -1,6 +1,5 @@
 package com.lukaslechner.coroutineusecasesonandroid.playground.coroutinebuilder
 
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
