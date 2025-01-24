19097b6fdb088410b1caee5c31df3fa882890173
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2025-01-24 15:13:20 +0600

    3 independent api call : Concurrent using async

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
@@ -3,6 +3,7 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase3
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class PerformNetworkRequestsConcurrentlyViewModel(
@@ -27,6 +28,32 @@ class PerformNetworkRequestsConcurrentlyViewModel(
     }
 
     fun performNetworkRequestsConcurrently() {
+
+        uiState.value = UiState.Loading
+
+       val oreoFeaturesDeferred = viewModelScope.async {
+            mockApi.getAndroidVersionFeatures(27)
+        }
+        val pieFeaturesDeferred = viewModelScope.async {
+            mockApi.getAndroidVersionFeatures(28)
+        }
+
+        val android10FeaturesDeferred = viewModelScope.async {
+            mockApi.getAndroidVersionFeatures(29)
+        }
+
+        viewModelScope.launch {
+            try {
+                val versionFeatures = listOf(
+                    oreoFeaturesDeferred.await(),
+                    pieFeaturesDeferred.await(),
+                    android10FeaturesDeferred.await()
+                )
+                uiState.value = UiState.Success(versionFeatures)
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error("Network Request failed")
+            }
+        }
 
     }
 }
